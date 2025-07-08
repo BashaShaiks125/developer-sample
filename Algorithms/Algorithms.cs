66e730807075ac621fc6e8eb83bd772f4f5ca777
BashaShaiks125 <77981804+BashaShaiks125@users.noreply.github.com>
2025-07-08 10:48:38 -0400

    Update Algorithms.cs
    BashaShaiks125/developer-sample -  implemented methods Formatseparator and Factorial

--- Algorithms/Algorithms.cs
+++ Algorithms/Algorithms.cs
@@ -4,8 +4,29 @@ namespace DeveloperSample.Algorithms
 {
     public static class Algorithms
     {
-        public static int GetFactorial(int n) => throw new NotImplementedException();
+        public static int GetFactorial(int n) 
+        {
+            if(n>0)
+            {
+                int fact= n;
+                for (int i = n - 1; i >= 1; i--)
+                    fact= fact*i;
+                return fact;
+            }
+            else throw new ArgumentException("Please provide valid number for factorial");
+        }
 
-        public static string FormatSeparators(params string[] items) => throw new NotImplementedException();
+        public static string FormatSeparators(params string[] items)
+        {
+            if(items == null || items.Length==0)
+                return string.Empty;
+            if(items.Length= 1)
+                return items[0];
+            if(items.Length == 2)
+                return $"{items[0]} and {items[1]}";
+
+            var str= string.Join(", ", items, 0, items.Length-1);
+            return $"{str} ,and {items[^1]}";
+        };
     }
 }
